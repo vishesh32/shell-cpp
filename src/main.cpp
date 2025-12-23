fe63fb9b6414cce2424f5eb18afc28f39ff0c7ee
vishesh32 <wish.mongia@gmail.com>
2025-12-23 23:48:19 +0000

    Added splitting command and args & echo command

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <string>
 enum class Command{
   Input,
+  Echo,
   Exit,
   Error
 };
 
-Command inputToCommand(std::string input){
-       if(input == "input") return Command::Input;
-  else if(input == "exit")  return Command::Exit;
+Command inputToCommand(std::string& input, std::string& args){
+  int command_idx =  input.find(" ");
+  std::string command = input.substr(0, command_idx);
+  args = input.substr(command_idx+1, input.size());
+  input = command;
+
+  if(command == "input") return Command::Input;
+  else if(command == "exit")  return Command::Exit;
+  else if(command == "echo")  return Command::Echo;
   else{return Command::Error; }
 }
 
@@ -20,12 +27,15 @@ int main() {
   while(true){
   std::cout << "$ ";
 
-  std::string input;
-  std::cin >> input;
-  Command command = inputToCommand(input);
+  std::string input, args;
+  std::getline(std::cin, input);
+  Command command = inputToCommand(input, args); // input now contains command as a string, everything after the command as args.
 
   switch(command){
   case(Command::Input):
+    break;
+  case(Command::Echo):
+    std::cout << args << std::endl;
     break;
   case(Command::Exit):
     return 0;
