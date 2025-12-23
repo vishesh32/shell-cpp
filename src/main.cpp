b28582fe06c76719187404690ec4789f0dd23e1c
vishesh32 <wish.mongia@gmail.com>
2025-12-23 20:59:48 +0000

    Added framework to execute commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
+enum class Command{
+  Input,
+  Error
+};
+
+Command inputToCommand(std::string input){
+  if(input == "input") return Command::Input;
+  else{return Command::Error; }
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  std::string input;
+  std::cin >> input;
+  Command command = inputToCommand(input);
+
+  switch(command){
+  case(Command::Input):
+    break;
+  case(Command::Error):
+    std::cout << '\n';
+    std::cout << input << " command not found";
+    break;
+  }
+
 }
